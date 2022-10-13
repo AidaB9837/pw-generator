0e5ca73867cfee75d33e1bf4bb0a01f7222e44a1
Aida Brugnone <99371449+AidaB9837@users.noreply.github.com>
2022-10-13 19:56:23 +0200

    feat: Add JS Code to set pw options and at the click event generate pw & show it

---
+++ js/script.js
@@ -0,0 +1,80 @@
+/* ----------- SET PASSWORD OPTIONS AND AT THE CLICK EVENT, GENERATE PASSWORD & SHOW IT START ----------- */
+const lengthSlider = document.querySelector(".pass-length input"); //input element about pw length
+const generateBtn = document.querySelector(".generate-btn"); //button to generate pw
+const options = document.querySelectorAll(".option input"); //option's checkbox
+const pwInput = document.querySelector(".input-box input"); //input element that show generated pw
+
+//obj of letters, numbers and symbols
+const characters = {
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numbers: "0123456789",
+  symbols: "^!$%&|[](){}:;.,*+-#@<>~",
+};
+
+const generatePassword = () => {
+  let basicPassword = "";
+  let generatedPassword = "";
+  let excDuplicate = false;
+  let pwLength = lengthSlider.value;
+
+  //for each option's checkbox
+  options.forEach((option) => {
+    // if checkbox is checked
+    if (option.checked) {
+      //if checkbox id isn't exc-duplicate && inc-spaces
+      if (option.id !== "exc-duplicate" && option.id !== "inc-spaces") {
+        //adding particular key value from characters obj to pw
+        basicPassword += characters[option.id];
+
+        //if checkbox id is inc-spaces
+      } else if (option.id === "inc-spaces") {
+        //adding space at the start & end of basicPassword
+        basicPassword += `   ${basicPassword}   `;
+      }
+
+      //else, pass true value to excDuplicate
+      else {
+        excDuplicate = true;
+      }
+    }
+  });
+
+  for (let i = 0; i < pwLength; i++) {
+    //get random generated characters from basicPassword
+    let generatedChar =
+      basicPassword[Math.floor(Math.random() * basicPassword.length)];
+
+    //if excDuplicate is true
+    if (excDuplicate) {
+      /*if the pw does not include the current random generated characters,
+        or  the current random generated character is equal to space " "*/
+      !generatedPassword.includes(generatedChar) || generatedChar == " "
+        ? //then, add random generated characters to generatedPassword
+          (generatedPassword += generatedChar)
+        : //else, decrement i by -1
+          i--;
+    }
+    //else, add random generated characters to generatedPassword
+    else {
+      generatedPassword += generatedChar;
+    }
+  }
+
+  //to show the generated password, pass it to pwInput value
+  pwInput.value = generatedPassword;
+};
+
+const updateSlider = () => {
+  // show password's length value as counter text
+  document.querySelector(".pass-length span").innerHTML = lengthSlider.value;
+
+  //run generatePassword fnc & show generated password
+  generatePassword();
+};
+
+updateSlider();
+
+lengthSlider.addEventListener("input", updateSlider);
+generateBtn.addEventListener("click", generatePassword);
+/* ------------ SET PASSWORD OPTIONS AND AT THE CLICK EVENT, GENERATE PASSWORD & SHOW IT END ------------ */
